5e62edfc3b61c1a6bc748ff72e6cb6b3414b613f
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-03-02 21:30:10 -0300

    Update video schedule in config.js

--- config.js
+++ config.js
@@ -4,24 +4,49 @@
 
 const schedule = [
     {
-        title: "Documentário - Introdução",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        title: "2ª Aula - ECT1 - O que é Ciência?",
+        url: "https://youtu.be/QT1hrrMcf4A?si=Ho7I41Gg7lM_2qw-",
         type: "youtube"
     },
     {
-        title: "Aula Magna - Vimeo",
-        url: "https://vimeo.com/148751763",
-        type: "vimeo"
+        title: "3ª Aula - ECT1 - Filosofia e História da Ciência ",
+        url: "https://youtu.be/dkpPr5myoqc?si=otqWRRumN5kIKjYT",
+        type: "youtube"
     },
     {
-        title: "Material Complementar - Drive",
-        url: "https://drive.google.com/file/d/SEU_ID_DO_ARQUIVO_AQUI/view",
+        title: "Contato (1997)",
+        url: "https://drive.google.com/file/d/1xVY5eC8ZbVUgwmwKZp8jZaRHvSU8tCBS/preview",
         type: "googledrive"
     },
     {
-        title: "Outro Vídeo YouTube",
-        url: "https://youtu.be/jfKfPfyJRdk",
+        title: "Gênios da Ciência da Antiga Grécia",
+        url: "https://youtu.be/abMcYvRj1nc?si=zKpMW5rKlprOaygW",
         type: "youtube"
-    }
-    // Para adicionar mais vídeos, copie um bloco {...}, coloque uma vírgula após o anterior e cole abaixo.
+    },
+{
+        title: "Toda a História das Civilizações Antigas",
+        url: "https://youtu.be/GFSoRGgdAfQ?si=nTWXhAt4bcertV6O",
+        type: "youtube"
+    },
+{
+        title: "Toda a História das Civilizações Antigas",
+        url: "https://youtu.be/X-N0nPb_iw4?si=ByBzdGjFv8_krAZl",
+        type: "youtube"
+    },  
+{
+        title: "Isaac Newton Gênios da Ciências",
+        url: "https://youtu.be/uac7S7Nyae8?si=ANFM4i-RqwqmQB1h",
+        type: "youtube"
+    }, 
+{
+        title: "Biografia de John Locke 1632: como um filósofo reescreveu a ideia de liberdade",
+        url: "https://youtu.be/nKRO6-lmqE8?si=tnV3oFf5aoCla8UE",
+        type: "youtube"
+    }, 
+{
+        title: "Ciência da Resistência",
+        url: "https://vimeo.com/421541483?fl=pl&fe=sh",
+        type: "vimeo"
+    },
+// Para adicionar mais vídeos, copie um bloco {...}, coloque uma vírgula após o anterior e cole abaixo.
 ];
