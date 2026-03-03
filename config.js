5433147af9af0885a79887ba64e76c5e9a54cfca
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-03-03 10:08:17 -0300

    Refactor schedule array in config.js

--- config.js
+++ config.js
@@ -3,64 +3,64 @@
 // Tipos suportados: 'youtube', 'vimeo', 'googledrive'
 
 const schedule = [
-    {
-        title: "2ª Aula - ECT1 - O que é Ciência?",
-        url: "https://youtu.be/QT1hrrMcf4A?si=Ho7I41Gg7lM_2qw-",
-        type: "youtube"
-    },
-    {
-        title: "3ª Aula - ECT1 - Filosofia e História da Ciência",
-        url: "https://youtu.be/dkpPr5myoqc?si=otqWRRumN5kIKjYT",
-        type: "youtube"
-    },
-    {
-        title: "Contato (1997)",
-        url: "https://drive.google.com/file/d/1xVY5eC8ZbVUgwmwKZp8jZaRHvSU8tCBS/view",
-        type: "googledrive"
-    },
-    {
-        title: "Gênios da Ciência da Antiga Grécia",
-        url: "https://youtu.be/abMcYvRj1nc?si=zKpMW5rKlprOaygW",
-        type: "youtube"
-    },
-    {
-        title: "Toda a História das Civilizações Antigas",
-        url: "https://youtu.be/GFSoRGgdAfQ?si=nTWXhAt4bcertV6O",
-        type: "youtube"
-    },
-    {
-        title: "Toda a História das Civilizações Antigas",
-        url: "https://youtu.be/X-N0nPb_iw4?si=ByBzdGjFv8_krAZl",
-        type: "youtube"
-    },  
-    {
-        title: "Isaac Newton Gênios da Ciências",
-        url: "https://youtu.be/uac7S7Nyae8?si=ANFM4i-RqwqmQB1h",
-        type: "youtube"
-    }, 
-    {
-        title: "Biografia de John Locke 1632: como um filósofo reescreveu a ideia de liberdade",
-        url: "https://youtu.be/nKRO6-lmqE8?si=tnV3oFf5aoCla8UE",
-        type: "youtube"
-    }, 
-    {
-        title: "3ª Aula - EUPPC - Por que devemos desinstalar a escola?",
-        url: "https://youtu.be/_Jj7tUgrFbA?si=FpbpRUnrSzk48n3f",
-        type: "youtube"
-    }
-{
-        title: "4ª Aula - EUPPC - Introdução ao documentário Schooling the World",
-        url: "https://youtu.be/wgpRePgjJUA?si=c9ZFsb4LvbhKmezc",
-        type: "youtube"
-    }
-{
-        title: "Filme recomendado: Capitão Fantásticos (2016)",
-        url: "https://drive.google.com/file/d/1rD6t3Iowex73KegNxye1vjA9OziCMvmc/view?usp=sharing",
-        type: "googledrive"
-    }
-{
-        title: "Escolarizando o Mundo [Schooling the World]",
-        url: "https://drive.google.com/file/d/1h2cBYrPyDV2BbcNfpSUrnpWwsmhoJccr/view?usp=sharing",
-        type: "googledrive"
-    }
+  {
+    title: "2ª Aula - ECT1 - O que é Ciência?",
+    url: "https://youtu.be/QT1hrrMcf4A?si=Ho7I41Gg7lM_2qw-",
+    type: "youtube"
+  },
+  {
+    title: "3ª Aula - ECT1 - Filosofia e História da Ciência",
+    url: "https://youtu.be/dkpPr5myoqc?si=otqWRRumN5kIKjYT",
+    type: "youtube"
+  },
+  {
+    title: "Contato (1997)",
+    url: "https://drive.google.com/file/d/1xVY5eC8ZbVUgwmwKZp8jZaRHvSU8tCBS/view",
+    type: "googledrive"
+  },
+  {
+    title: "Gênios da Ciência da Antiga Grécia",
+    url: "https://youtu.be/abMcYvRj1nc?si=zKpMW5rKlprOaygW",
+    type: "youtube"
+  },
+  {
+    title: "Toda a História das Civilizações Antigas",
+    url: "https://youtu.be/GFSoRGgdAfQ?si=nTWXhAt4bcertV6O",
+    type: "youtube"
+  },
+  {
+    title: "Toda a História das Civilizações Antigas",
+    url: "https://youtu.be/X-N0nPb_iw4?si=ByBzdGjFv8_krAZl",
+    type: "youtube"
+  },
+  {
+    title: "Isaac Newton Gênios da Ciências",
+    url: "https://youtu.be/uac7S7Nyae8?si=ANFM4i-RqwqmQB1h",
+    type: "youtube"
+  },
+  {
+    title: "Biografia de John Locke 1632: como um filósofo reescreveu a ideia de liberdade",
+    url: "https://youtu.be/nKRO6-lmqE8?si=tnV3oFf5aoCla8UE",
+    type: "youtube"
+  },
+  {
+    title: "3ª Aula - EUPPC - Por que devemos desinstalar a escola?",
+    url: "https://youtu.be/_Jj7tUgrFbA?si=FpbpRUnrSzk48n3f",
+    type: "youtube"
+  },
+  {
+    title: "4ª Aula - EUPPC - Introdução ao documentário Schooling the World",
+    url: "https://youtu.be/wgpRePgjJUA?si=c9ZFsb4LvbhKmezc",
+    type: "youtube"
+  },
+  {
+    title: "Filme recomendado: Capitão Fantásticos (2016)",
+    url: "https://drive.google.com/file/d/1rD6t3Iowex73KegNxye1vjA9OziCMvmc/view?usp=sharing",
+    type: "googledrive"
+  },
+  {
+    title: "Escolarizando o Mundo [Schooling the World]",
+    url: "https://drive.google.com/file/d/1h2cBYrPyDV2BbcNfpSUrnpWwsmhoJccr/view?usp=sharing",
+    type: "googledrive"
+  }
 ];
