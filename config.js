4549f36eb90ab782a579aeb7ef50c9cb8fb4adff
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-03-02 21:45:21 -0300

    Refactor video schedule in config.js

--- config.js
+++ config.js
@@ -9,13 +9,13 @@ const schedule = [
         type: "youtube"
     },
     {
-        title: "3ª Aula - ECT1 - Filosofia e História da Ciência ",
+        title: "3ª Aula - ECT1 - Filosofia e História da Ciência",
         url: "https://youtu.be/dkpPr5myoqc?si=otqWRRumN5kIKjYT",
         type: "youtube"
     },
     {
         title: "Contato (1997)",
-        url: "https://drive.google.com/file/d/1xVY5eC8ZbVUgwmwKZp8jZaRHvSU8tCBS/preview",
+        url: "https://drive.google.com/file/d/1xVY5eC8ZbVUgwmwKZp8jZaRHvSU8tCBS/view",
         type: "googledrive"
     },
     {
@@ -23,30 +23,29 @@ const schedule = [
         url: "https://youtu.be/abMcYvRj1nc?si=zKpMW5rKlprOaygW",
         type: "youtube"
     },
-{
+    {
         title: "Toda a História das Civilizações Antigas",
         url: "https://youtu.be/GFSoRGgdAfQ?si=nTWXhAt4bcertV6O",
         type: "youtube"
     },
-{
+    {
         title: "Toda a História das Civilizações Antigas",
         url: "https://youtu.be/X-N0nPb_iw4?si=ByBzdGjFv8_krAZl",
         type: "youtube"
     },  
-{
+    {
         title: "Isaac Newton Gênios da Ciências",
         url: "https://youtu.be/uac7S7Nyae8?si=ANFM4i-RqwqmQB1h",
         type: "youtube"
     }, 
-{
+    {
         title: "Biografia de John Locke 1632: como um filósofo reescreveu a ideia de liberdade",
         url: "https://youtu.be/nKRO6-lmqE8?si=tnV3oFf5aoCla8UE",
         type: "youtube"
     }, 
-{
+    {
         title: "Ciência da Resistência",
-        url: "https://vimeo.com/421541483?fl=pl&fe=sh",
+        url: "https://vimeo.com/421541483",
         type: "vimeo"
-    },
-// Para adicionar mais vídeos, copie um bloco {...}, coloque uma vírgula após o anterior e cole abaixo.
+    }
 ];
