63ef6a943767e075568bc71ebedf3ab430282749
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-03-04 18:20:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ const schedule = [
     {
   title: "Contato (1997)",
   type: "googledrive",
-  url: "https://drive.google.com/uc?export=download&id=1xVY5eC8ZbVUgwmwKZp8jZaRHvSU8tCBS/view"
+  url: "https://drive.google.com/uc?export=download&id=1xVY5eC8ZbVUgwmwKZp8jZaRHvSU8tCBS/view?usp=sharing"
 },
     {
         title: "Discovery Science As 100 Maiores Descobertas da História - Biologia",
@@ -54,6 +54,7 @@ const schedule = [
     type: "youtube"
     }
 ];
+
 
 
 
