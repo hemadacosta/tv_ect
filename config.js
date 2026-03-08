34f9498bc46e667af50f9284efd022ae1f92bd84
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-03-08 14:39:44 -0300

    Update config.js

--- config.js
+++ config.js
@@ -29,8 +29,7 @@ const schedule = [
         type: "youtube"
     },
     {
-        title: "Soylent Green (1973) Official Trailer - Charlton Heston, Edward G Robinson - 👀 Assista ao filme completo nos slides do Aprender3 da 2ª Aula - ECT1"
- - ECT1",
+        title: "Soylent Green (1973) Official Trailer - Charlton Heston, Edward G Robinson - 👀 Assista ao filme completo nos slides do Aprender3 da 2ª Aula - ECT1",
         url: "https://youtu.be/N_jGOKYHxaQ?si=Ng3VNawHr54Gy0Uv",
         type: "youtube"
     },
@@ -95,6 +94,7 @@ const schedule = [
     type: "youtube"
     }
 ];
+
 
 
 
