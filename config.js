e58eb80e584923f42a079ab14598ae3f86ac8156
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-03-29 13:44:26 -0300

    Refactor video schedule format in config.js

--- config.js
+++ config.js
@@ -15,19 +15,19 @@ const schedule = [
     },
     {
         title: "BNCC: Ciência da Natureza",
-      url: "https://youtu.be/7fp_-_bJWro"
-      type: "youtube"
+        url: "https://youtu.be/7fp_-_bJWro",
+        type: "youtube"
     },
-    {
+    {       
         title: "Como a BNCC organizou o Ensino Fundamental",
-    url: "https://youtu.be/W4U4qd2JejQ?si=biZLZEu78H7O_ulQ",
-    type: "youtube"
+        url: "https://youtu.be/W4U4qd2JejQ?si=biZLZEu78H7O_ulQ",
+        type: "youtube"
     },
     {
-        title: "BNCC: Competências e Habilidades: Educação Infantil e Ensino Fundamental",
-    url: "https://youtu.be/8t0QtyAXvWc?si=gt-1jWWUyal6Q3ws",
-    type: "youtube"
-    }, 
+        title: "BNCC: Competências e Habilidades, Educação Infantil e Ensino Fundamental",
+        url: "https://youtu.be/8t0QtyAXvWc?si=gt-1jWWUyal6Q3ws",
+        type: "youtube"
+    },
     {
         title: "Muito Além do Peso",
         url: "https://youtu.be/8UGe5GiHCT4?si=kVyf3WgdFVDKd056",
@@ -47,7 +47,7 @@ const schedule = [
         title: "Mudança de Paradigma - Thomas Kuhn",
         url: "https://youtu.be/3i0jEzV_JC8",
         type: "youtube"
-    },
+    },  
     {
         title: "Resumo PCN Ciências Naturais - Anos Iniciais",
         url: "https://youtu.be/sqp08D2J_K4?si=AZh_qcNo0czOn68O",
@@ -67,10 +67,15 @@ const schedule = [
         title: "Cosmos Carl Sagan Ep.2 - Uma Voz na Sinfonia Cósmica",
     url: "https://youtu.be/nEax6pI4nMc?si=Tv9PU_lR60J8GHtj",
     type: "youtube"
+    }, 
+    {
+        title: "Enigma, O Jogo da Imitação",
+    url: "https://youtu.be/lRid96uWpqo?si=1YAC_vbGGxapG8Oq",
+    type: "youtube"
     },
     {
-        title: "Enigma: O Jogo da Imitação",
-    url: "https://youtu.be/lRid96uWpqo?si=1YAC_vbGGxapG8Oq",
+        title: "Descartes Cartesius (1974)",
+    url: "https://youtu.be/fMUQZHRco-g?si=lt01_Jkq_u3pVj2A",
     type: "youtube"
     },
     {
@@ -78,4 +83,4 @@ const schedule = [
     url: "https://youtu.be/UnSA27a00To?si=DBZtBKbVAmtOHVeF",
     type: "youtube"
     }
-];
+ ];
