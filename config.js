e46307742def6c6e7778c8a5314588e465f49426
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-03-29 12:17:06 -0300

    Refactor video schedule format in config.js

--- config.js
+++ config.js
@@ -13,37 +13,37 @@ const schedule = [
         url: "https://youtu.be/EFMtk5qex80?si=9QkYezdtgF4Q1hLc",
         type: "youtube"
     },
-{
+    {
         title: "BNCC: Ciência da Natureza",
       url: "https://youtu.be/7fp_-_bJWro"
       type: "youtube"
-      },
-{
+    },
+    {
         title: "Como a BNCC organizou o Ensino Fundamental",
     url: "https://youtu.be/W4U4qd2JejQ?si=biZLZEu78H7O_ulQ",
     type: "youtube"
     },
-{
+    {
         title: "BNCC: Competências e Habilidades: Educação Infantil e Ensino Fundamental",
     url: "https://youtu.be/8t0QtyAXvWc?si=gt-1jWWUyal6Q3ws",
     type: "youtube"
     }, 
-{
+    {
         title: "Muito Além do Peso",
         url: "https://youtu.be/8UGe5GiHCT4?si=kVyf3WgdFVDKd056",
         type: "youtube"
     },
-{
+    {
         title: "A Queda - as últimas horas de Hitler",
         url: "https://youtu.be/kVzCBX2AToc?si=ACN9PX6LHL4BA0be",
         type: "youtube"
     },
-      {
+    {
         title: "O Óleo de Lorenzo (1992) - Trailer (Assista ao filme completo na apresentação da 5ª Aula - ECT1",
         url: "https://youtu.be/GLcx7xMMBoM?si=_PYLdmDZ16RLwuYz",
         type: "youtube"
     },
-      {
+    {
         title: "Mudança de Paradigma - Thomas Kuhn",
         url: "https://youtu.be/3i0jEzV_JC8",
         type: "youtube"
@@ -58,7 +58,7 @@ const schedule = [
         url: "https://youtu.be/978BUOV5Swc?si=CskLm-8fUX847Ueg",
         type: "youtube"
     },
-      {
+    {
         title: "Novos Paradigmas na Ciência",
     url: "https://youtu.be/XG_O8gW-vCg?si=UnFPTT1AGeJbK6pc",
     type: "youtube"
@@ -73,7 +73,7 @@ const schedule = [
     url: "https://youtu.be/lRid96uWpqo?si=1YAC_vbGGxapG8Oq",
     type: "youtube"
     },
-{
+    {
         title: "Albert Einstein",
     url: "https://youtu.be/UnSA27a00To?si=DBZtBKbVAmtOHVeF",
     type: "youtube"
