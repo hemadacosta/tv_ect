a771d56551377f15e4f1ff957f9b5756a0863912
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-03-31 18:15:50 -0300

    Refactor video schedule format in config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ const schedule = [
     {
   title: "Uma mente brilhante (2001)",
   url: "https://player.vimeo.com/video/382446611?h=65d4fbb945"
-  type: "vimeo"
+  type: "vimeo",
   }
     {
         title: "Resumo PCN Ciências Naturais - Anos Iniciais",
