672462a187a576988cfd88b6c8259e964f80394b
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-04-05 19:07:41 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 const schedule = [
     {
-        title: "6ª Aula - O papel dos materiais curriculares no processo de ensino e aprendizagem de conhecimentos científicos – Bens culturais para o ensino de ciência e diretrizes para o trabalho final", //49:40
+        title: "6ª Aula - O papel dos materiais curriculares no processo de ensino e aprendizagem de conhecimentos científicos – Bens culturais para o ensino de ciência e diretrizes para o trabalho final",
         url: "https://youtu.be/mUINTFy7w7Y",
         type: "youtube"
     },
@@ -14,22 +14,22 @@ const schedule = [
         type: "youtube"
       },
     {
-        title: "7ª Aula - ECT1 - O Ensino de Ciências e a proposição de Sequências de Ensino Investigativo", //48:19
+        title: "7ª Aula - ECT1 - O Ensino de Ciências e a proposição de Sequências de Ensino Investigativo",
         url: "https://youtu.be/AbXHq5WT3eY",
         type: "youtube"
     },
     {
-        title: "Currículo e Materiais Curriculares", //24:24
+        title: "Currículo e Materiais Curriculares",
       url: "https://youtu.be/dMeOepkonQ4?si=OayGXeKW7fkZHNLx"
       type: "youtube"
     },
     {       
-    title: "Sequência de Ensino Investigativo (SEI)", //15:19
+    title: "Sequência de Ensino Investigativo (SEI)",
     url: "https://youtu.be/Eglil-b5D4o?si=QgEVN0eF_eZHEVn4",
     type: "youtube"
     },
     {
-        title: "Saiba como promover o letramento científico", //7:27
+        title: "Saiba como promover o letramento científico",
         url: "https://youtu.be/BfZOktW6CvE?si=BXflKrKhan-9A41Y",
         type: "youtube"
     },
@@ -39,7 +39,7 @@ const schedule = [
         type: "youtube"
       },
     {
-    title: "Escritores da Liberdade (2007)" //2:02:57
+    title: "Escritores da Liberdade (2007)",
     url: "https://player.vimeo.com/video/1102925342?h=f0f14ace87",
     type: "vimeo"
     },
@@ -49,7 +49,7 @@ const schedule = [
         type: "youtube"
       },
     {
-        title: "Formação de professores de Ciências - Ana Maria Pessoa de Carvalho", //16:47
+        title: "Formação de professores de Ciências - Ana Maria Pessoa de Carvalho",
         url: "https://youtu.be/IMyfqxACezE?si=0CH3_ojQ4C0eNhq4",
         type: "youtube"
     },
@@ -59,12 +59,12 @@ const schedule = [
         type: "youtube"
       },
     {
-       title: "Uma mente brilhante (2001)", //2:14:58
+       title: "Uma mente brilhante (2001)",
         url: "https://player.vimeo.com/video/382446611?h=65d4fbb945",
         type: "vimeo" 
     },
     {
-        title: "Planejamento para o Ensino de Biologia - Por que Ensino de Ciências por Investigação?", //19:19
+        title: "Planejamento para o Ensino de Biologia - Por que Ensino de Ciências por Investigação?",
         url: "https://youtu.be/kM6WgTzkN1A?si=S4t7fyzgHrugPVYY",
         type: "youtube"
     },
@@ -74,12 +74,12 @@ const schedule = [
         type: "youtube"
       },
     {
-        title: "Fundamentos e Práticas no Ensino de Ciências da Natureza - O Ensino por investigação: leitura", //17:51
+        title: "Fundamentos e Práticas no Ensino de Ciências da Natureza - O Ensino por investigação: leitura",
         url: "https://youtu.be/7B_PuKM-Oig?si=D8pESwebVwoe3KoA",
         type: "youtube"
     },  
     {
-        title: "Como funciona a produção científica", //8:08
+        title: "Como funciona a produção científica",
         url: "https://youtu.be/EbhNfd3tQtY?si=S7hv-k4_K2HfeDRG",
         type: "youtube"
     },
@@ -89,7 +89,7 @@ const schedule = [
         type: "youtube"
       },
         {
-        title: 'Viagem Insólita (1987) - Trailer | <a href="https://drive.google.com/file/d/1atqH1JreRBJNOrbT9E1kSzDkbiBLcv3H/preview" target="_blank">🎬 Assistir Filme</a>', //1:54
+        title: 'Viagem Insólita (1987) - Trailer | <a href="https://drive.google.com/file/d/1atqH1JreRBJNOrbT9E1kSzDkbiBLcv3H/preview" target="_blank">🎬 Assistir Filme</a>',
     url: "https://youtu.be/InWhsN3jMp8?si=hbYVl5xOxYYJIFd8&t=6",
     type: "youtube"
     },
@@ -99,12 +99,12 @@ const schedule = [
         type: "youtube"
       },
     {
-        title: "Enigma, O Jogo da Imitação", //2:23:28
+        title: "Enigma, O Jogo da Imitação",
     url: "https://youtu.be/lRid96uWpqo?si=1YAC_vbGGxapG8Oq",
     type: "youtube"
     },
     {
-    title: "Quando o pior acontece - catástrofes naturais e mudanças climáticas", //54:40
+    title: "Quando o pior acontece - catástrofes naturais e mudanças climáticas",
     url: "https://geo.dailymotion.com/player.html?video=x8cl1cg",
     type: "dailymotion"
     }
