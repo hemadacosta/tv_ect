a6f456cbb59da39827404b83b9eb6ec86a03b9c7
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-08-05 11:45:06 -0300

    Refactor video schedule in config.js
    Removed duplicate entries and added new video links to the schedule.

--- config.js
+++ config.js
@@ -13,6 +13,46 @@ const schedule = [
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
         type: "youtube"
       },
+    {
+        title: "A Ciência em Foco",
+        url: "https://youtu.be/HfBwsEbjrKo?si=7NdHqkHLsYmE8_Mm",
+        type: "youtube"
+    },
+    {
+        title: "UNIVESP - Ensino de Ciências: métodos e técnicas",
+        url: "https://youtu.be/qynHlQkj93Y?si=7COrQtS8mxgFipDf",
+        type: "youtube"
+    },
+    {
+        title: "Leonardo Da Vinci - O homem que inventou o futuro",
+        url: "https://youtu.be/3iGPclknn_8?si=v9MIOxosUIXX3Bra",
+        type: "youtube"
+    },
+    {
+        title: "Ensino de Ciências: história e situação atual",
+        url: "https://youtu.be/nh1ruCC0yA4?si=yQJ5SZYFdcNc-Rtg",
+        type: "youtube"
+    },
+    {
+        title: "A Impactante Origem da Vida e o Nascimento da Terra",
+        url: "https://youtu.be/6JZVUe_hiPA?si=oB_WYLk7rvdKQKkU",
+        type: "youtube"
+    },
+    {
+        title: "Formação de professores de Ciências - Ana Maria Pessoa de Carvalho",
+        url: "https://youtu.be/IMyfqxACezE?si=BvJSo2d2smj0ZUFL",
+        type: "youtube"
+    },
+    {
+        title: "Ary Mergulhão - A importância do Ensino de Ciências na educação infantil",
+    url: "https://youtu.be/FbY6kx2P-xk?si=RIYIABLXnOSemWNp",
+    type: "youtube"
+    },
+    {
+        title: "Conexão Professor - O ensino de Ciências por investigação nos Anos Iniciais",
+    url: "https://www.youtube.com/embed/Wbyyx4Rud54?si=hbfIqki67ZD4e8VT",
+    type: "youtube"
+    }, 
 {
     title: "Inherit The Wind (1999) - O Vento Será Tua Herança",
     url: "https://youtu.be/NYCfo4hVolQ?si=YEwzO1uk_31NeTbx",
@@ -28,41 +68,16 @@ const schedule = [
     url: "https://youtu.be/6QubWEM7mKg?si=gZmk9FhGFrwxdKpl",
     type: "youtube"
     },
- {
-        title: "2ª Aula - ECT1 - O que é Ciência?",
-        url: "https://youtu.be/QT1hrrMcf4A?si=Ho7I41Gg7lM_2qw-",
-        type: "youtube"
-    },
 {
     title: "TV ECT",
     url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
     type: "youtube"
       },
 {
-        title: "3ª Aula - ECT1 - Filosofia e História da Ciência",
-        url: "https://youtu.be/dkpPr5myoqc?si=otqWRRumN5kIKjYT",
-        type: "youtube"
-    },
-{
-        title: "Contato (1997)",
-        url: "https://drive.google.com/file/d/1xVY5eC8ZbVUgwmwKZp8jZaRHvSU8tCBS/preview",
-        type: "googledrive"
-      },
-{
         title: "TV ECT",
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
         type: "youtube"
       },
-{
-        title: "Soylent Green, no mundo de 2020 (1973)",
-        url: "https://drive.google.com/file/d/1X8PS-GS_SLmqlh79qE1xgGJVPmtyXz2O/preview",
-        type: "googledrive"
-      },
-{
-        title: "4ª Aula - ECT1 - O ensino de ciências para os primeiros anos da Educação Básica",
-        url: "https://youtu.be/4Dc2lEfnir4?si=PvVNwdAdSnRsU2Lh",
-        type: "youtube"
-    },
 {
         title: "TV ECT",
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
@@ -78,99 +93,34 @@ const schedule = [
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
         type: "youtube"
       },
-{
-        title: "A Queda - as últimas horas de Hitler",
-        url: "https://youtu.be/kVzCBX2AToc?si=ACN9PX6LHL4BA0be",
-        type: "youtube"
-    },
-{
+   {
         title: "TV ECT",
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
         type: "youtube"
       },
-    {
-        title: "O Óleo de Lorenzo (1992)",
-        url: "https://drive.google.com/file/d/10_ps5BlJZQN5VVpqgplOEAIc3t11XXp5/preview",
-        type: "googledrive"
-      },
-{
-        title: "O Matemático - Adventures of a Mathematician (2020)",
-        url: "https://youtu.be/978BUOV5Swc?si=CskLm-8fUX847Ueg",
-        type: "youtube"
-    },
-{
-        title: "TV ECT",
-        url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
-        type: "youtube"
-      },
-{
-        title: "6ª Aula - O papel dos materiais curriculares no processo de ensino e aprendizagem de conhecimentos científicos – Bens culturais para o ensino de ciência e diretrizes para o trabalho final",
-        url: "https://youtu.be/mUINTFy7w7Y",
-        type: "youtube"
-    },
     {
         title: "TV ECT",
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
         type: "youtube"
       },
     {
-        title: "7ª Aula - ECT1 - O Ensino de Ciências e a proposição de Sequências de Ensino Investigativo",
-        url: "https://youtu.be/AbXHq5WT3eY",
-        type: "youtube"
-    },
-{
         title: "TV ECT",
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
         type: "youtube"
       },
 {
-     title: "Escritores da Liberdade (2007)",
-     url: "https://player.vimeo.com/video/1102925342?h=f0f14ace87",
-     type: "vimeo"
-    },
-{
-        title: "Uma mente brilhante (2001)",
-        url: "https://player.vimeo.com/video/382446611?h=65d4fbb945",
-        type: "vimeo"
-},
-{
         title: "TV ECT",
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
         type: "youtube"
 },
-{
-        title: "Viagem Insólita (1987)",
-        url: "https://drive.google.com/file/d/1atqH1JreRBJNOrbT9E1kSzDkbiBLcv3H/previeww",
-        type: "googledrive"
-      },
-{
-        title: "8ª Aula - Contribuições de Gaston Bachelard ao Ensino de Ciências",
-        url: "https://youtu.be/q4zYWLfHKMU",
-        type: "youtube"
-    },
     {
         title: "TV ECT",
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
         type: "youtube"
       },
-      { 
-  title: "Estrelas Além do Tempo (2016)",
-  url: "https://drive.google.com/file/d/1KahcP2-bxQr1uzKAMg91bGGSgrYw2Bcp/preview",
-  type: "googledrive",
-  },
-{
-        title: "Quase Deuses (2004)",
-        url: "https://youtu.be/2b4peUNL710?si=BzokINNMj_bSM8FG",
-        type: "youtube"
-},
-{
+      {
         title: "TV ECT",
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
         type: "youtube"
-},
-{
-        title: "Interestelar (2014)",
-        url: "https://drive.google.com/file/d/1oimF3SLY4fBCoc9exvIyglUACgv4t5-Q/preview",
-        type: "googledrive"
-      }
+}
 ];
