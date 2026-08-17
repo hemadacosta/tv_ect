53e702a3617a73483353cfbc9fac5619df5705fa
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-08-17 06:41:58 -0300

    Revise video schedule in config.js
    Updated the video schedule with new entries and removed duplicates.

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 
 const schedule = [
     {
-        title: "1ª Aula - ECT1 - Apresentação da disciplina",
-        url: "https://youtu.be/T-FeG_CdTF0?si=sAPKRXS8Uc-6YToD",
+        title: "2ª Aula - ECT1 - O que é Ciência?",
+        url: "https://youtu.be/QT1hrrMcf4A?si=Ho7I41Gg7lM_2qw-",
         type: "youtube"
     },
 {
@@ -13,50 +13,50 @@ const schedule = [
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
         type: "youtube"
       },
-    {
-        title: "A Ciência em Foco",
-        url: "https://youtu.be/HfBwsEbjrKo?si=7NdHqkHLsYmE8_Mm",
+        {
+        title: "3ª Aula - ECT1 - Filosofia e História da Ciência",
+        url: "https://youtu.be/dkpPr5myoqc?si=otqWRRumN5kIKjYT",
         type: "youtube"
     },
     {
-        title: "UNIVESP - Ensino de Ciências: métodos e técnicas",
-        url: "https://youtu.be/qynHlQkj93Y?si=7COrQtS8mxgFipDf",
+        title: "Memória Paranaense: Newton Freire-Maia - Parte 1",
+url: "https://youtu.be/_1omhivT5zc?si=HlGLL2vZIvZtEIYt",
+type: "youtube"
+    },
+    {
+       title: "Memória Paranaense: Newton Freire-Maia - Parte 2",
+url: "https://youtu.be/amlWGuwMZxc?si=XdkqSg8QVdfxulF1",
+type: "youtube"
+    },
+    {
+       title: "Contato (1997) - Trailer (Obs.: Assista o filme completo na apresentação da aula)",
+        url: "https://youtu.be/CVm-dewknqs?si=AmFTmJx8PZyTrN_-",
         type: "youtube"
     },
     {
-        title: "Leonardo Da Vinci - O homem que inventou o futuro",
-        url: "https://youtu.be/3iGPclknn_8?si=v9MIOxosUIXX3Bra",
+        title: "Soylent Green (1973)  - Trailer (Obs.: Assista o filme completo na apresentação da aula)",
+        url: "https://youtu.be/N_jGOKYHxaQ?si=QNao_la03-qzStoQ",
         type: "youtube"
     },
     {
-        title: "Ensino de Ciências: história e situação atual",
-        url: "https://youtu.be/nh1ruCC0yA4?si=yQJ5SZYFdcNc-Rtg",
+        title: "Discovery Science As 100 Maiores Descobertas da História - Biologia",
+        url: "https://youtu.be/UB6I6SBLEBc?si=YwJyiigwUdzjhNg4",
         type: "youtube"
     },
     {
-        title: "A Impactante Origem da Vida e o Nascimento da Terra",
-        url: "https://youtu.be/6JZVUe_hiPA?si=oB_WYLk7rvdKQKkU",
+        title: "Discovery Science As 100 Maiores Descobertas da História - Química",
+        url: "https://youtu.be/L5Y5kybGPOs?si=NcaFKbn-DIPeT_Nx",
         type: "youtube"
     },
     {
-        title: "Formação de professores de Ciências - Ana Maria Pessoa de Carvalho",
-        url: "https://youtu.be/IMyfqxACezE?si=BvJSo2d2smj0ZUFL",
+        title: "Cosmos (Carl Sagan) | Episódio 01 - Os Limites do Oceano Cósmico",
+        url: "https://youtu.be/N1CQQLSJfwQ?si=jIoCRK8OdDyUlTlG",
         type: "youtube"
-    },
-    {
-        title: "Ary Mergulhão - A importância do Ensino de Ciências na educação infantil",
-    url: "https://youtu.be/FbY6kx2P-xk?si=RIYIABLXnOSemWNp",
-    type: "youtube"
-    },
-    {
-        title: "Conexão Professor - O ensino de Ciências por investigação nos Anos Iniciais",
-    url: "https://www.youtube.com/embed/Wbyyx4Rud54?si=hbfIqki67ZD4e8VT",
-    type: "youtube"
     }, 
 {
-    title: "Inherit The Wind (1999) - O Vento Será Tua Herança",
-    url: "https://youtu.be/NYCfo4hVolQ?si=YEwzO1uk_31NeTbx",
-    type: "youtube"
+    title: "Toda a História das Civilizações Antigas",
+        url: "https://youtu.be/X-N0nPb_iw4?si=ByBzdGjFv8_krAZl",
+        type: "youtube"
     },
 {
     title: "TV ECT",
@@ -64,9 +64,9 @@ const schedule = [
         type: "youtube"
       },
 {
-        title: "Never Cry Wolf (1983)",
-    url: "https://youtu.be/6QubWEM7mKg?si=gZmk9FhGFrwxdKpl",
-    type: "youtube"
+        title: "A Incrível METAMORFOSE | O Mistério da Transformação na Natureza",
+        url: "https://youtu.be/0BSvnesnNdk?si=fycrwI-dkhBNSwgw",
+        type: "youtube"
     },
 {
     title: "TV ECT",
@@ -74,9 +74,9 @@ const schedule = [
     type: "youtube"
       },
 {
-        title: "TV ECT",
-        url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
-        type: "youtube"
+        title: "Nicolau Copérnico Gênios da Ciência",
+    url: "https://youtu.be/o3pVDA2WPY4?si=bDf0_QF2_Q07wm8E",
+    type: "youtube"
       },
 {
         title: "TV ECT",
@@ -84,9 +84,9 @@ const schedule = [
         type: "youtube"
       },
     {
-        title: "5ª Aula - ECT1 - Revoluções Científicas e Ciência Normal na Sala de Aula",
-        url: "https://youtu.be/EFMtk5qex80?si=9QkYezdtgF4Q1hLc",
-        type: "youtube"
+        title: "Johannes Kepler Gênios da Ciência",
+    url: "https://youtu.be/K1AWf_sRy-8?si=TDVAndMLt21oepGw",
+    type: "youtube"
     },
 {
         title: "TV ECT",
@@ -94,9 +94,9 @@ const schedule = [
         type: "youtube"
       },
    {
-        title: "TV ECT",
-        url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
-        type: "youtube"
+        title: "Galileu e sua batalha pelo céu!",
+    url: "https://youtu.be/psAwszDxLvc?si=QZs0yl77TlidDQfw",
+    type: "youtube"
       },
     {
         title: "TV ECT",
@@ -104,9 +104,9 @@ const schedule = [
         type: "youtube"
       },
     {
-        title: "TV ECT",
-        url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
-        type: "youtube"
+        title: "Descartes Cartesius (1974)",
+    url: "https://youtu.be/fMUQZHRco-g?si=lt01_Jkq_u3pVj2A",
+    type: "youtube"
       },
 {
         title: "TV ECT",
@@ -114,13 +114,24 @@ const schedule = [
         type: "youtube"
 },
     {
-        title: "TV ECT",
-        url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
-        type: "youtube"
+        title: "O Legado de Pitágoras!",
+    url: "https://youtu.be/_LFygNg5lnk?si=UunK1wEZpe-APWTF",
+    type: "youtube"
       },
       {
         title: "TV ECT",
         url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
         type: "youtube"
+      },
+    {
+{
+title: "Isaac Newton Gênios da Ciência",
+    url: "https://youtu.be/uac7S7Nyae8?si=kYbqJ9EHT9zyQe9z",
+    type: "youtube"
+    },
+{
+        title: "Construindo o Planeta Terra!",
+    url: "https://youtu.be/RavBvn1BFj0?si=ZLQSjr1vaOcRHnpP",
+    type: "youtube"
 }
 ];
