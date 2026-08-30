7e89452857fe40f8dff1cf121ba14469b914620a
hemadacosta <156354202+hemadacosta@users.noreply.github.com>
2026-08-30 16:27:00 -0300

    Refactor video schedule in config.js

--- config.js
+++ config.js
@@ -4,108 +4,108 @@
 
 const schedule = [
     {
-       title: "6ª Aula - O papel dos materiais curriculares no processo de ensino e aprendizagem de conhecimentos científicos – Bens culturais para o ensino de ciência e diretrizes para o trabalho final",
-        url: "https://youtu.be/mUINTFy7w7Y",
-        type: "youtube"
-    },
-{
-        title: "TV ECT",
-        url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
-        type: "youtube"
-      },
-        {
-        title: "7ª Aula - ECT1 - O Ensino de Ciências e a proposição de Sequências de Ensino Investigativo",
-        url: "https://youtu.be/AbXHq5WT3eY",
-        type: "youtube"
+    title: "6ª Aula - O papel dos materiais curriculares no processo de ensino e aprendizagem de conhecimentos científicos",
+    url: "https://youtu.be/mUINTFy7w7Y",
+    type: "youtube"
     },
     {
-       title: "Currículo e Materiais Curriculares",
-      url: "https://youtu.be/dMeOepkonQ4?si=OayGXeKW7fkZHNLx",
-      type: "youtube"
+    title: "TV ECT",
+    url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
+    type: "youtube"
     },
     {
-       title: "Sequência de Ensino Investigativo (SEI)",
+    title: "7ª Aula - ECT1 - O Ensino de Ciências e a proposição de Sequências de Ensino Investigativo",
+    url: "https://youtu.be/AbXHq5WT3eY",
+    type: "youtube"
+    },
+    {
+    title: "Currículo e Materiais Curriculares",
+    url: "https://youtu.be/dMeOepkonQ4?si=OayGXeKW7fkZHNLx",
+    type: "youtube"
+    },
+    {
+    title: "Sequência de Ensino Investigativo SEI",
     url: "https://youtu.be/Eglil-b5D4o?si=QgEVN0eF_eZHEVn4",
     type: "youtube"
     },
-{   
-        title: "TV ECT",
-        url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
-        type: "youtube"
-      },
+    {   
+    title: "TV ECT",
+    url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
+    type: "youtube"
+    },
     {
-       title: "Escritores da Liberdade (2007)",
+    title: "Escritores da Liberdade (2007)",
     url: "https://player.vimeo.com/video/1102925342?h=f0f14ace87",
     type: "vimeo"
     },
     {     
     title: "Formação de professores de Ciências - Ana Maria Pessoa de Carvalho", 
-        url: "https://youtu.be/IMyfqxACezE?si=0CH3_ojQ4C0eNhq4",
-        type: "youtube"
+    url: "https://youtu.be/IMyfqxACezE?si=0CH3_ojQ4C0eNhq4",
+    type: "youtube"
     },
-{   
-        title: "TV ECT",
-        url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
-        type: "youtube"
-      },
+    {   
+    title: "TV ECT",
+    url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
+    type: "youtube"
+    },
     {
-        title: title: "Uma mente brilhante (2001)",
-        url: "https://player.vimeo.com/video/382446611?h=65d4fbb945",
-        type: "vimeo"
+    title: title: "Uma mente brilhante (2001)",
+    url: "https://player.vimeo.com/video/382446611?h=65d4fbb945",
+    type: "vimeo"
     },
     {     
-    title: "Planejamento para o Ensino de Biologia - Aula 13 - Por que Ensino de Ciências por Investigação?",
-        url: "https://youtu.be/kM6WgTzkN1A?si=S4t7fyzgHrugPVYY",
-        type: "youtube"
+    title: "Planejamento para o Ensino de Biologia - Aula 13 - Por que Ensino de Ciências por Investigação",
+    url: "https://youtu.be/kM6WgTzkN1A?si=S4t7fyzgHrugPVYY",
+    type: "youtube"
     },
     {
-        title: "Fundamentos e Práticas no Ensino de Ciências da Natureza - O Ensino por investigação: leitura",
-        url: "https://youtu.be/7B_PuKM-Oig?si=D8pESwebVwoe3KoA",
-        type: "youtube"
+    title: "Fundamentos e Práticas no Ensino de Ciências da Natureza - O Ensino por investigação leitura",
+    url: "https://youtu.be/7B_PuKM-Oig?si=D8pESwebVwoe3KoA",
+    type: "youtube"
     },
-{   
-        title: "TV ECT",
-        url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
-        type: "youtube"
-      },
-    {
-        title: "Como funciona a produção científica",
-        url: "https://youtu.be/EbhNfd3tQtY?si=S7hv-k4_K2HfeDRG",
-        type: "youtube"
+    {   
+    title: "TV ECT",
+    url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
+    type: "youtube"
     },
     {
-        title: 'Viagem Insólita (1987) - Trailer | <a href="https://drive.google.com/file/d/1atqH1JreRBJNOrbT9E1kSzDkbiBLcv3H/preview" target="_blank">🎬 Assistir Filme</a>',
+    title: "Como funciona a produção científica",
+    url: "https://youtu.be/EbhNfd3tQtY?si=S7hv-k4_K2HfeDRG",
+    type: "youtube"
+    },
+    {
+    title: 'Viagem Insólita (1987) - Trailer | <a href="https://drive.google.com/file/d/1atqH1JreRBJNOrbT9E1kSzDkbiBLcv3H/preview" target="_blank">🎬 Assistir Filme</a>',
     url: "https://youtu.be/InWhsN3jMp8?si=hbYVl5xOxYYJIFd8&t=6",
     type: "youtube"
     },
-{
+    {
     title: "Viagem Insólita (1987)",
     url: "https://drive.google.com/file/d/1atqH1JreRBJNOrbT9E1kSzDkbiBLcv3H/preview",
     type: "googledrive"
-},
-{
+    },
+    {
     title: "Enigma: O Jogo da Imitação (2014)",
     url: "https://www.youtube.com/embed/lRid96uWpqo",
     type: "youtube"
     },
-{
-    title: "TV ECT",
-        url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
-        type: "youtube"
-      },
-{
-        title: "Quando o pior acontece - catástrofes naturais e mudanças climáticas",
-    url: "https://geo.dailymotion.com/player.html?video=x8cl1cg",
-    type: "dailymotion"
-    },
-{
-        title: "Cosmos Carl Sagan Ep. 3 - A Harmonia dos Mundos",
-    url: "https://youtu.be/kgsz7AGNIyU?si=lXvjL84eu6Vv7pep",
-    type: "youtube"
-    },
-{
+    {
     title: "TV ECT",
     url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
     type: "youtube"
-}
-];
+    },
+    {
+    title: "Quando o pior acontece - catástrofes naturais e mudanças climáticas",
+    url: "https://geo.dailymotion.com/player.html?video=x8cl1cg",
+    type: "dailymotion"
+    },
+    {
+    title: "Cosmos Carl Sagan Ep. 3 - A Harmonia dos Mundos",
+    url: "https://youtu.be/kgsz7AGNIyU?si=lXvjL84eu6Vv7pep",
+    type: "youtube"
+    },
+    {
+    title: "TV ECT",
+    url: "https://youtu.be/7t_vyWPGD2s?si=Zn70raI1AmqbZHxP",
+    type: "youtube"
+    }
+    ];
